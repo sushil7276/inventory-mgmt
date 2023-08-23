ae1bf06369ae22900bcdd1713f38e003347b77eb
Sushil <shil7276@gmail.com>
2023-08-23 12:40:16 +0530

    product controller created

---
+++ index.js
@@ -0,0 +1,15 @@
+import express from "express";
+import ProductController from "./src/controllers/product.controller.js";
+
+const server = express();
+
+// Create an instance of ProductController
+const productController = new ProductController();
+
+server.get("/", productController.getProducts);
+
+server.use(express.static("src/views"));
+
+server.listen(3400, () => {
+  console.log(`server is started on 3400`);
+});
