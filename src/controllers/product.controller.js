4ad80d3c47aa4bc3656c151abaa7c66149602edf
Sushil <shil7276@gmail.com>
2023-08-24 23:36:06 +0530

    delete console log

--- src/controllers/product.controller.js
+++ src/controllers/product.controller.js
@@ -4,7 +4,6 @@ import ProductModel from "../models/product.model.js";
 export default class ProductController {
   getProducts(req, res) {
     let products = ProductModel.get();
-    console.log(products);
     // return res.sendFile(
     //   path.join(path.resolve(), "src", "views", "products.ejs")
     // );
