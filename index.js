0e9947fc9424d80050d954ba7da06f4b6fefed09
Sushil <shil7276@gmail.com>
2023-08-24 23:04:46 +0530

    Create Layout :  1. install express-ejs-layouts for creating layout and render dynamically pages on runtime

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 import express from "express";
 import ProductController from "./src/controllers/product.controller.js";
 import path from "path";
+import ejsLayouts from "express-ejs-layouts";
 
 const server = express();
 
 // Setup view engin settings
 server.set("view engine", "ejs");
 server.set("views", path.join(path.resolve(), "src", "views"));
+
+server.use(ejsLayouts);
 
 // Create an instance of ProductController
 const productController = new ProductController();
